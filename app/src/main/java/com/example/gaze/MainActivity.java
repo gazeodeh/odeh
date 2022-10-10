3fc8d2207d77ab6e2bdd348b9197deb0f1b24074
gaze odeh <gazeodeh2005@gmail.com>
2022-10-10 14:16:31 +0300

    init

--- app/src/main/java/com/example/gaze/MainActivity.java
+++ app/src/main/java/com/example/gaze/MainActivity.java
@@ -11,4 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    myImgView.setBackgroundResource(R.drawable.gaze11.jpg);
 }
